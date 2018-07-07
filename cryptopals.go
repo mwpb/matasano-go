ef8f6e91a95a74f0be705c20d2c3e3499744e67c
Matthew Burke <matthewpatrickburke@gmail.com>
2018-07-07 12:33:46 -0600

    Trimmed files.

--- cryptopals.go
+++ cryptopals.go
@@ -1,7 +1,6 @@
 package cryptopals
 
 import (
-	"encoding/hex"
 	"math"
 )
 
@@ -15,25 +14,4 @@ func xor(bytes1 []byte, bytes2 []byte) []byte {
 		xor[i] = bytes1[i1] ^ bytes2[i2]
 	}
 	return xor
-}
-
-func hexXORSingle(hexes string, r byte) string {
-	bytes, _ := hex.DecodeString(hexes)
-	xor := make([]byte, 0)
-	for i := 0; i < len(bytes); i++ {
-		// log.Println(string(hexes[2*i]), string(hexes[2*i+1]), bytes[i], r, bytes[i]^r)
-		xor = append(xor, bytes[i]^r)
-	}
-	// log.Println(xor)
-	return string(xor[:])
-}
-
-func hexXORAlpha(hexes string) map[byte]string {
-	plaintexts := make(map[byte]string, 0)
-	for i := 0; i < 255; i++ {
-		xor := hexXORSingle(hexes, byte(i))
-		plaintexts[byte(i)] = xor
-	}
-	// log.Println(plaintexts)
-	return plaintexts
 }
