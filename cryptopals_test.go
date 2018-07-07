b18c92fcfdf4369737ca1abff4e81ca60d06c277
Matthew Burke <matthewpatrickburke@gmail.com>
2018-07-07 15:33:36 -0600

    Added new frequency table.

--- cryptopals_test.go
+++ cryptopals_test.go
@@ -1,10 +1,17 @@
 package cryptopals
 
 import (
+	"bufio"
 	"encoding/base64"
 	"encoding/hex"
+	"log"
+	"os"
 	"testing"
 )
+
+func dummy() {
+	log.Println("dummy")
+}
 
 func TestS1C1(t *testing.T) {
 	bytes, _ := hex.DecodeString("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
@@ -33,5 +40,28 @@ func TestS1C3(t *testing.T) {
 	ans, _ := minScore(all)
 	if string(ans) != "Cooking MC's like a pound of bacon" {
 		t.Errorf("s1c3 failed: output is %v", ans)
+	}
+}
+
+func TestS1C4(t *testing.T) {
+	file, _ := os.Open("4.txt")
+	scanner := bufio.NewScanner(file)
+	lines := make([][]byte, 0)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Bytes())
+	}
+	all := make([][]byte, len(lines)*256)
+	for i := 0; i < 256; i++ {
+		for j, bytes := range lines {
+			ar := [1]byte{byte(i)}
+			all[i*256+j] = xor(bytes, ar[:])
+		}
+	}
+	ans, score := minScore(all)
+	log.Println(string(ans))
+	log.Println(score)
+	log.Println(len(ef))
+	if len(ans) < 0 {
+		t.Errorf("s1c4 failed: output is %v", ans)
 	}
 }
