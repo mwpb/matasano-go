614bfed1a77027e9fb16bec11b52efb1bb531ada
Matthew Burke <matthewpatrickburke@gmail.com>
2018-07-08 13:29:58 -0600

    Completed s1c5.

--- cryptopals_test.go
+++ cryptopals_test.go
@@ -75,3 +75,12 @@ func TestS1C4(t *testing.T) {
 		t.Errorf("s1c4 failed: output is %s and key is %s", ans, currentKey)
 	}
 }
+
+func TestS1C5(t *testing.T) {
+	block := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+	xor := xor(block, []byte("ICE"))
+	ans := hex.EncodeToString(xor)
+	if ans != "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" {
+		t.Errorf("s1c5 failed: output is %s", ans)
+	}
+}
