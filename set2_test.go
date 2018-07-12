fc5b83dc07060c7b1637ce277d7241b7d1708e64
Matthew Burke <matthewpatrickburke@gmail.com>
2018-07-12 11:01:09 -0600

    Messy solution to c10.

--- set2_test.go
+++ set2_test.go
@@ -1,7 +1,6 @@
 package cryptopals
 
 import (
-	"bytes"
 	"crypto/aes"
 	"encoding/base64"
 	"io/ioutil"
@@ -9,27 +8,36 @@ import (
 	"testing"
 )
 
-func TestS2C9(t *testing.T) {
-	original := []byte("YELLOW SUBMARINE")
-	ans := pad(original, 20)
-	expected := append([]byte("YELLOW SUBMARINE"), []byte{4, 4, 4, 4}...)
-	if bytes.Equal(ans, expected) == false {
-		t.Errorf("s2c1 failed: output is %v", ans)
-	}
-}
+// func TestS2C9(t *testing.T) {
+// 	original := []byte("YELLOW SUBMARINE")
+// 	ans := pad(original, 20)
+// 	expected := append([]byte("YELLOW SUBMARINE"), []byte{4, 4, 4, 4}...)
+// 	if bytes.Equal(ans, expected) == false {
+// 		t.Errorf("s2c1 failed: output is %v", ans)
+// 	}
+// }
 
 func TestS2C10(t *testing.T) {
 	ciphertext, _ := ioutil.ReadFile("10.txt")
 	ciphertext, _ = base64.StdEncoding.DecodeString(string(ciphertext))
-	ciphertext = ciphertext[:16]
-	log.Println(ciphertext)
+	ciphertext = pad(ciphertext, 16)
 
 	block, _ := aes.NewCipher([]byte("YELLOW SUBMARINE"))
 
-	dstBlock := make([]byte, 16)
-	block.Decrypt(dstBlock, ciphertext)
+	dst := make([]byte, len(ciphertext))
+	prevCipherBlock := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	log.Println(len(prevCipherBlock))
+	for i := 0; i < len(ciphertext)/16; i++ {
+		dstBlock := make([]byte, 16)
+		cipherblock := ciphertext[i*16 : (i+1)*16]
+		block.Decrypt(dstBlock, cipherblock)
+		for j := 0; j < 16; j++ {
+			dst[16*i+j] = xor(prevCipherBlock, dstBlock)[j]
+		}
+		prevCipherBlock = cipherblock
+	}
 
-	ans := dstBlock
+	ans := dst
 	log.Println(string(ans))
 	if len(ans) < 0 {
 		t.Errorf("s2c1 failed: output is %v", ans)
