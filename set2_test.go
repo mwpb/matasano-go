99c2c9a43293c59d9abb74e288646fb8a8fe96f2
Matthew Burke <matthewpatrickburke@gmail.com>
2018-07-13 11:17:40 -0600

    Figured out go lang random int generation.

--- set2_test.go
+++ set2_test.go
@@ -1,13 +1,15 @@
 package cryptopals
 
 import (
-	"bytes"
-	"crypto/aes"
-	"encoding/base64"
-	"io/ioutil"
+	"crypto/rand"
 	"log"
+	"math/big"
 	"testing"
 )
+
+func dummy2() {
+	log.Println("HI")
+}
 
 // func TestS2C9(t *testing.T) {
 // 	original := []byte("YELLOW SUBMARINE")
@@ -18,44 +20,59 @@ import (
 // 	}
 // }
 
-func TestS2C10(t *testing.T) {
-	input, _ := ioutil.ReadFile("10.txt")
-	input, _ = base64.StdEncoding.DecodeString(string(input))
-	ciphertext := pad(input, 16)
-	log.Println(ciphertext)
+// func TestS2C10(t *testing.T) {
+// 	input, _ := ioutil.ReadFile("10.txt")
+// 	input, _ = base64.StdEncoding.DecodeString(string(input))
+// 	ciphertext := pad(input, 16)
+// 	block, _ := aes.NewCipher([]byte("YELLOW SUBMARINE"))
+// 	dst := make([]byte, len(ciphertext))
+// 	prevCipherBlock := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+// 	for i := 0; i < len(ciphertext)/16; i++ {
+// 		dstBlock := make([]byte, 16)
+// 		cipherblock := ciphertext[i*16 : (i+1)*16]
+// 		block.Decrypt(dstBlock, cipherblock)
+// 		for j := 0; j < 16; j++ {
+// 			dst[16*i+j] = xor(prevCipherBlock, dstBlock)[j]
+// 		}
+// 		prevCipherBlock = cipherblock
+// 	}
+// 	prevCipherBlock = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+// 	original := make([]byte, len(dst))
+// 	for i := 0; i < len(dst)/16; i++ {
+// 		afterXOR := xor(prevCipherBlock, dst[i*16:(i+1)*16])
+// 		newBlock := make([]byte, 16)
+// 		block.Encrypt(newBlock, afterXOR)
+// 		for j := 0; j < 16; j++ {
+// 			original[16*i+j] = newBlock[j]
+// 		}
+// 		prevCipherBlock = newBlock
+// 	}
+// 	ans := original
+// 	if bytes.Equal(ciphertext, original) == false {
+// 		t.Errorf("s2c1 failed: output is %v", ans)
+// 	}
+// }
 
-	block, _ := aes.NewCipher([]byte("YELLOW SUBMARINE"))
-
-	dst := make([]byte, len(ciphertext))
-	prevCipherBlock := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	for i := 0; i < len(ciphertext)/16; i++ {
-		dstBlock := make([]byte, 16)
-		cipherblock := ciphertext[i*16 : (i+1)*16]
-		block.Decrypt(dstBlock, cipherblock)
-		for j := 0; j < 16; j++ {
-			dst[16*i+j] = xor(prevCipherBlock, dstBlock)[j]
-		}
-		prevCipherBlock = cipherblock
+func TestS2C11(t *testing.T) {
+	rand1, _ := rand.Int(rand.Reader, big.NewInt(2))
+	encryptionMethod := rand1.Int64()
+	rand2, _ := rand.Int(rand.Reader, big.NewInt(5))
+	preLength := rand2.Int64() + 5
+	rand3, _ := rand.Int(rand.Reader, big.NewInt(5))
+	postLength := rand3.Int64() + 5
+	pre := make([]byte, preLength)
+	post := make([]byte, postLength)
+	rand.Read(pre)
+	rand.Read(post)
+	if encryptionMethod == 0 {
+		log.Println("encode using ecb")
+	} else {
+		iv := make([]byte, 16)
+		rand.Read(iv)
+		log.Println("encode using cbc")
 	}
-	// log.Println("dst")
-	// log.Println(dst)
-
-	prevCipherBlock = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	original := make([]byte, len(dst))
-	for i := 0; i < len(dst)/16; i++ {
-		afterXOR := xor(prevCipherBlock, dst[i*16:(i+1)*16])
-		newBlock := make([]byte, 16)
-		block.Encrypt(newBlock, afterXOR)
-		for j := 0; j < 16; j++ {
-			original[16*i+j] = newBlock[j]
-		}
-		prevCipherBlock = newBlock
-	}
-	// original = original[16:]
-	log.Println("orig")
-	log.Println(original)
-	ans := original
-	if bytes.Equal(ciphertext, original) == false {
+	ans := encryptionMethod
+	if ans < 0 {
 		t.Errorf("s2c1 failed: output is %v", ans)
 	}
 }
